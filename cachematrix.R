9763732402c5b93d6bf5b9efd93f775157641c5e
shivanibalyan-1 <61529770+shivanibalyan-1@users.noreply.github.com>
2021-05-17 08:39:20 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(inversee) s <<- inversee
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+##
+## Same here, changed "mean" to "solve" and "m" to "s"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  solve(x)
 }
